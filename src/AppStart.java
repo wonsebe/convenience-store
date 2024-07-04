bf1c2d9eaa8baad5f83b9b601fdae504efce55cb
tjwlsvy <tjwlsvy1@naver.com>
2024-07-04 15:10:57 +0900

    24.07.04 tnwjd

--- src/AppStart.java
+++ src/AppStart.java
@@ -3,15 +3,6 @@ import java.util.Scanner;
 
 public class AppStart {
     public static void main(String[] args) {
-        // 사용자 입력을 받기 위한 Scanner 객체 생성
-        Scanner scan = new Scanner(System.in);
-
-        // 현재 턴을 저장하는 변수 초기화
-        int turn = 1;
-
-        // PcController 객체 생성
-        PcController pcController = new PcController();
-
 
     } // main 함수 끝
 } // AppStart 클래스 끝
