6e342b595d818744099cb4f774669314a40456b5
tjwlsvy <tjwlsvy1@naver.com>
2024-07-09 18:44:43 +0900

    24.07.08

--- src/AppStart.java
+++ src/AppStart.java
@@ -1,5 +1,6 @@
 // ProductView 클래스를 import
 
+import view.BoardView;
 import view.ProductView;
 
 // 애플리케이션의 시작점을 정의하는 클래스
@@ -7,6 +8,8 @@ public class AppStart {
     // 프로그램의 진입점 main 메서드
     public static void main(String[] args) {
         // ProductView의 싱글톤 인스턴스를 가져와 index() 메서드 호출
-        ProductView.getInstance().start();
+        //ProductView.getInstance().start();
+        BoardView.getInstance().Bprinter();
+
     } // main 함수 끝
 } // AppStart 클래스 끝
